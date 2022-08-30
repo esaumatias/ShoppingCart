3fc320a8e3d80e535cf32412732839fe16465ee7
Esau Matias <esaumatias@Mac-mini-de-Esau.local>
2022-08-30 15:49:01 -0300

    feat(CardProducts): adiciona botao de favoritar produtos e botao de adicionar no carrinho de compras

--- app/src/Components/CardProducts.js
+++ app/src/Components/CardProducts.js
@@ -1,8 +1,17 @@
 import React, { useContext } from 'react';
 import AppContext from '../Context/AppContext';
 import { Card, Col, Row } from 'react-bootstrap';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import Favorite from '@mui/icons-material/Favorite';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
 import '../Style/CardProducts.css'
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
 
 function CardProducts() {
     const { productsList } = useContext(AppContext);
@@ -14,10 +23,11 @@ function CardProducts() {
             <Col key={idx}>
               <section className="containerCard">
                 <div className="sectionTitle">
-                  <Card.Img
-                    variant="top"
-                    src={product.thumbnail}
-                    style={{ width: "150px" }}
+                  <CardMedia
+                    component="img"
+                    sx={{ width: 151 }}
+                    image={ product.thumbnail }
+                    alt="Live from space album cover"
                   />
                 </div>
                 <div className="sectionDetails">
@@ -25,15 +35,22 @@ function CardProducts() {
                     <p>{product.title}</p>
                     <Card.Text>
                       <h3>{`R$ ${product.price}`}</h3>
-                      <strong>{`em ${product.installments.quantity}x de R$ ${
-                        product.installments.amount
-                      } ${
-                        product.installments.rate === 0 ? "sem juros" : ""
-                      }`}</strong>
+                      <Typography variant="subtitle1" color="text.secondary" component="div">
+                        <strong>{`em ${product.installments.quantity}x de R$ ${
+                          product.installments.amount
+                        } ${
+                          product.installments.rate === 0 ? "sem juros" : ""
+                        }`}</strong>
+                      </Typography>
                     </Card.Text>
                   </Card.Body>
+                  <Button variant="contained" color="success">
+                    Adicionar
+                  </Button>
                 </div>
-                <div className="sectionFavorite">coração</div>
+                <div className="sectionFavorite">
+                  <Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+                </div>
               </section>
             </Col>
           ))
