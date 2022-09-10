c176fd2377d1ceb0f5f90aef1202547c9f256b4b
Esau Matias <esaufreirema]tias@gmail.com>
2022-09-10 13:37:32 -0300

    feat(Header) - implementa badge com icone de um carrinho de compras com total de produtos no carrinho

--- app/src/Components/Header.js
+++ app/src/Components/Header.js
@@ -1,12 +1,26 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React, { useContext } from 'react';
+import AppContext from '../Context/AppContext';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import ListItemIcon from '@mui/material/ListItemIcon';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
 import { lightGreen } from '@mui/material/colors';
+import { styled } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
 
 function Header() {
+  const { itemsCart } = useContext(AppContext);
+
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+      right: -3,
+      top: 13,
+      border: `2px solid ${theme.palette.background.paper}`,
+      padding: '0 4px',
+    },
+  }));
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
@@ -21,7 +35,11 @@ function Header() {
         </Navbar.Collapse>
         <ListItemIcon>
           <Link to="/cart">
-            <ShoppingCartIcon sx={{ color: lightGreen[500] }} fontSize="medium" />
+            <IconButton aria-label="cart">
+              <StyledBadge badgeContent={ itemsCart.length } color="primary">
+                <ShoppingCartIcon sx={{ color: lightGreen[500] }}/>
+              </StyledBadge>
+            </IconButton>
           </Link>
         </ListItemIcon>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
